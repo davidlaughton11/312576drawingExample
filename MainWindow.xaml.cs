52f07fdf23dd9fb54ffd8c15d214aa37ad2a45ca
davidlaughton11 <47399940+davidlaughton11@users.noreply.github.com>
2019-03-20 10:04:55 -0400

    Add files via upload

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _312576drawingExample
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            Rectangle r = new Rectangle();
+            r.Height = 100;
+            r.Width = 100;
+            r.Fill = Brushes.Salmon;
+            canvas.Children.Add(r);
+            Canvas.SetTop(r, 10);
+            Canvas.SetLeft(r, 10);
+        }
+    }
+}
